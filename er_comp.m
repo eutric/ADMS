54732820efe459f49cc928a9addc5d1a1f61adb6
eutric <eutric03@gmail.com>
2026-04-28 13:43:19 +0200

    boh

--- er_comp.m
+++ er_comp.m
@@ -2,12 +2,11 @@ function [err] = er_comp(Gnum,Gexp,x)
 %Gnum è cell array di function handles, Gexp è un cell harray di valori della FRF di dimensione numero
 %frequenze su cui ricostruisco FRF e numero punti dalle quali estraggo i
 %dati [100,3]
-err=0;
 [m,n]=size(Gnum);
-err=0;
+err=[];
 for i =1:m
     for j=1:n
-        err=[err;Gnum{i,j}(x)-Gexp{i,j}];
+        err=[err;(Gnum{i,j}(x)-Gexp{i,j})];
     end
 end
 
