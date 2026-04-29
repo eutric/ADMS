7045ac91afe859748a59dc121f3f1ea5d45644d6
Gabu-debug <best.giacognolo@gmail.com>
2026-04-29 11:57:05 +0200

    punto 5 - done

--- assignment1.m
+++ assignment1.m
@@ -91,7 +91,7 @@ for i = 1:length(om_vect)
     dets(i)=det(H(g(om_vect(i))));
 end
 abs_det = abs(dets);
-[amp, i_nat] = findpeaks(-abs(dets));
+[~, i_nat] = findpeaks(-abs(dets));
 
 figure
 semilogy(f_vect, abs_det)
@@ -101,24 +101,28 @@ grid on
 % computing mode shapes
 figure
 xx=linspace(0,beam.L,1000);
+labels = cell(1, length(i_nat));
 for i =1:length(i_nat)
     mode(i).i_nat=i_nat(i);
     mode(i).OM=om_vect(i_nat(i));
     mode(i).G=g(mode(i).OM);
     mode(i).HH=H(mode(i).G);
     mode(i).f=mode(i).OM/2/pi;
-    mode(i).X=[1;-inv(mode(i).HH(2:end,2:end))*mode(i).HH(2:end,1)];
+    mode(i).X=[1;-mode(i).HH(2:end,2:end)\mode(i).HH(2:end,1)];
     mode(i).phi=@(x)mode(i).X(1)*cos(mode(i).G.*x) + mode(i).X(2)*sin(mode(i).G.*x)+ mode(i).X(3)*cosh(mode(i).G.*x) + mode(i).X(4)*sinh(mode(i).G.*x);
 
     plot(xx, mode(i).phi(xx))
+    tag = sprintf('Mode %d', i);
+    labels{i} = {tag};
     hold on
 end
 grid on
+legend([labels{:}])
 
 % compute FRF
 %assume 
-x_in=.5; % m
-x_out=1.2;
+x_in=.2; % m
+x_out=.6;
 
 [G,mode]=FRF_num(mode,x_in,x_out,beam,-1); % L'ultimo parametro è la fase/contro fase di spostamento e forza, la prof li ha di segno opposto
 % figure
@@ -130,7 +134,7 @@ x_out=1.2;
 % grid on
 % 
 % test FRF in other point, compute FRF_num
-x_out_vect=[0.2,0.5,1.2];
+x_out_vect=[0.2,0.7,1.2];
 [FRF1,mode1]=FRF_num(mode,x_in,x_out_vect(1),beam);
 [FRF2,mode2]=FRF_num(mode,x_in,x_out_vect(2),beam);
 [FRF3,mode3]=FRF_num(mode,x_in,x_out_vect(3),beam);
@@ -142,9 +146,9 @@ FRF_list={FRF1,FRF2,FRF3};
 
 % x = [om_i,csi_1,A1,A2,A3,RL1,RL2,RL3,RH1,RH2,RH3];
 
-range = .5; %Hz
+range = 1; %Hz
 
-[x_sols, G_num_list, x_0s, n, errs] = mode_i_num(FRF_list, range, f_vect);
+[x_sols, G_num_list, x_0s, n] = mode_i_num(FRF_list, range, f_vect, mode);
 
 G_in_out = cell(n.ch,1);
 for jj=1:n.ch
@@ -209,34 +213,42 @@ plot(f_vect, angle(FRF1(om_vect)));
 % 3. The modeshape is then known for each output point: phi_ij
 
 % 1 - x_j = x_k 
-x_j = .5; % Location of the input
+x_j = .2; % Location of the input
 
-[FRF_jj{1}] = FRF_num(mode, x_j, x_j, beam, -1);
-[x_sols, G_num_list, ~, n] = mode_i_num(FRF_jj, 1, f_vect);
+[FRF_jj{1}] = FRF_num(mode, x_j, x_j, beam, 1);
+[x_sols, ~, ~, ~] = mode_i_num(FRF_jj, 1, f_vect, mode);
 
-phi_ij = zeros(n.modes,1);
-for ii = 1:n.modes
-    phi_ij(ii) = sqrt( imag( -2*x_sols(2,ii)*x_sols(1,ii)^2*G_num_list{ii,1}(x_sols(:,ii),x_sols(1,ii)) ) );
-end
+% phi_ij = zeros(n.modes,1);
+Ajj = x_sols(3,:);
+phi_ij = sqrt(real(Ajj));
+% for ii = 1:n.modes
+%     phi_ij(ii) = sqrt( 1i*2*x_sols(2,ii)*x_sols(1,ii)^2*G_num_list{ii,1}(x_sols(:,ii),x_sols(1,ii)) );
+% end
 
 n_outs = 8;
 x_ks = linspace(0,beam.L, n_outs); % 6 output points, equally spaced
 
 FRFs = cell(n_outs, 1);
 for ii = 1:n_outs
-    FRFs{ii} = FRF_num(mode, x_j, x_ks(ii), beam, 1);
+    FRFs{ii} = FRF_num(mode, x_j, x_ks(ii), beam, -1);
 end
 
-[x_sols, G_num_list, x_0s, n] = mode_i_num(FRFs, 1, f_vect);
+[x_sols, G_num_list, x_0s, n] = mode_i_num(FRFs, 1, f_vect, mode);
 
 % For each solution, i have the estimated A, from wich i can deriva 
 % phi_ij
-phi_ik = zeros(n.modes, n.ch);
+Ajk = x_sols(3:2+n.ch,:);
+phi_ik = zeros(n.ch, n.modes);
+
 for ii=1:n.modes
-    for jj=1:n.ch
-        phi_ik(ii,jj) = G_num_list{ii,jj}(x_sols(:,ii),x_sols(1,ii))*2*x_sols(2,ii)*x_sols(1,ii)^2/phi_ij(ii);
-    end
+    phi_ik(:,ii) = real(Ajk(:,ii))./phi_ij(ii);
 end
+
+% for ii=1:n.modes
+%     for jj=1:n.ch
+%         phi_ik(ii,jj) = 1i*G_num_list{ii,jj}(x_sols(:,ii),x_sols(1,ii))*2*x_sols(2,ii)*x_sols(1,ii)^2/phi_ij(ii);
+%     end
+% end
 
 % => A is matrix n.ch x n.modes, the element jj,ii is the i-th mode value
 % on point j of the i-th modeshape
@@ -248,7 +260,66 @@ for ii=1:n.modes
     subplot(2,2,ii)
     plot(xx, mode(ii).phi(xx));
     hold on
-    scatter(x_ks, phi_ik(ii,:))
+    scatter(x_ks, phi_ik(:,ii))
     grid on
 end
 
+%% what happens around .6?
+clear
+close all
+clc
+
+% Beam Data
+
+beam.L=1.200;  % m
+beam.h=0.008;  % m
+beam.b=0.040;  % m
+beam.rho=2700; % kg/m^3
+beam.E=68e9;   % Pa - Alluminio
+beam.m=beam.rho*beam.h*beam.b; % kg/m
+
+beam.J=beam.h^3*beam.b/12;     % m^4;
+
+% Freq data
+f_max = 200;  % Hz
+f_res = 10000; % Hz
+f_vect = linspace(0, f_max, f_res);
+om_vect = 2*pi.*f_vect; % rad/s
+g = @(om) (beam.m*om.^2/beam.E/beam.J).^(1/4);
+
+%build the H matrix
+H=@(g) [1 0 1 0;
+    0 g 0 g;
+    -beam.E*beam.J.*g.^2.*cos(g*beam.L) -beam.E*beam.J.*g.^2.*sin(g*beam.L) beam.E*beam.J.*g.^2.*cosh(g*beam.L) beam.E*beam.J.*g.^2.*sinh(g*beam.L);
+    beam.E*beam.J.*g.^3.*sin(g*beam.L) -beam.E*beam.J*g.^3.*cos(g*beam.L) beam.E*beam.J*g.^3.*sinh(g*beam.L) beam.E*beam.J*g.^3.*cosh(g*beam.L) ];
+dets=zeros(length(om_vect),1);
+for i = 1:length(om_vect)
+    dets(i)=det(H(g(om_vect(i))));
+end
+abs_det = abs(dets);
+[amp, i_nat] = findpeaks(-abs(dets));
+
+labels = cell(1, length(i_nat));
+for i =1:length(i_nat)
+    mode(i).i_nat=i_nat(i);
+    mode(i).OM=om_vect(i_nat(i));
+    mode(i).G=g(mode(i).OM);
+    mode(i).HH=H(mode(i).G);
+    mode(i).f=mode(i).OM/2/pi;
+    mode(i).X=[1;-inv(mode(i).HH(2:end,2:end))*mode(i).HH(2:end,1)];
+    mode(i).phi=@(x)mode(i).X(1)*cos(mode(i).G.*x) + mode(i).X(2)*sin(mode(i).G.*x)+ mode(i).X(3)*cosh(mode(i).G.*x) + mode(i).X(4)*sinh(mode(i).G.*x);
+end
+
+
+x_in = .8;
+x_out = .3429;
+
+[FRF, mode] = FRF_num(mode, x_in, x_out, beam, -1);
+
+figure
+subplot(2,1,1)
+semilogy(f_vect, abs(FRF(om_vect)))
+grid
+subplot(2,1,2)
+plot(f_vect, angle(FRF(om_vect)))
+grid
--- mode_i_num.m
+++ mode_i_num.m
@@ -1,4 +1,4 @@
-function [x_sols, G_num_list, x_0s, n, err_vec] = mode_i_num(FRF_list, range, f_vect)
+function [x_sols, G_num_list, x_0s, n] = mode_i_num(FRF_list, range, f_vect, mode)
 
 OM_vect = 2*pi.*f_vect;
 f_res = length(f_vect);
@@ -6,49 +6,44 @@ f_max = f_vect(end);
 f_in_range = floor(f_res/f_max);
 % x = [om_i,csi_1,A1,A2,A3,RL1,RL2,RL3,RH1,RH2,RH3];
 
-n_ch = length(FRF_list);
-G_exp = zeros(length(OM_vect), n_ch);
-for jj = 1:n_ch
+n.ch = length(FRF_list);
+n.modes = length(mode);
+G_exp = zeros(length(OM_vect), n.ch);
+for jj = 1:n.ch
     G_exp(:,jj) = FRF_list{jj}(OM_vect)';
-    [mode(jj).ampl,mode(jj).loc] = findpeaks(abs(G_exp(:,jj))); % Amplitude and location of the resonance peaks
+    % [mode(jj).ampl,mode(jj).loc] = findpeaks(abs(G_exp(:,jj))); % Amplitude and location of the resonance peaks
 end % mode_ampl(ii,jj) is mode ii of pair jj
-n_modes = length(mode(1).loc);
 
-x_sols = zeros(2 + 3*n_ch, n_modes);
+x_sols = zeros(2 + 3*n.ch, n.modes);
 x_0s = x_sols;
 % MODE ii
-G_num_list = cell(n_modes, n_ch);
-for ii = 1:n_modes % I do this, for the same mode of different pairs
-    x0_ii = zeros (2 + 3*n_ch, 1);
-    x0_ii(1) = 2*pi*f_vect(mode(1).loc(ii)); % This should be the same for all the pairs
+G_num_list = cell(n.modes, n.ch);
+for ii = 1:n.modes % I do this, for the same mode of different pairs
+    x0_ii = zeros (2 + 3*n.ch, 1);
+    x0_ii(1) = mode(ii).OM; % This should be the same for all the pairs
 
-    d_dom = (angle(FRF_list{1}(OM_vect(mode(1).loc(ii)+1)))- ...
-            angle(FRF_list{1}(OM_vect(mode(1).loc(ii)))))/ (OM_vect(mode(1).loc(ii)+1) ...
-            -OM_vect(mode(1).loc(ii))); % derivate of phase of mode ii for the jj pair
+    d_dom = (angle(FRF_list{1}(OM_vect(mode(1).i_nat+1)))- ...
+            angle(FRF_list{1}(OM_vect(mode(1).i_nat))))/ (OM_vect(mode(1).i_nat+1) ...
+            -OM_vect(mode(1).i_nat)); % derivate of phase of mode ii for the jj pair
             % Estimation of x0
 
-    x0_ii(2) = -1/(d_dom*OM_vect(mode(1).loc(ii)));
+    x0_ii(2) = -1/(d_dom*OM_vect(mode(1).i_nat));
 
     err_vec_ii = @(x) 0;
-    for jj = 1:n_ch % For each pair given (channel)
-        G_num_list{ii, jj} = @(x, OM) x(2+jj)./(-OM.^2 + 1i*2*x(2)*x(1).*OM+x(1)^2) + x(2+jj+n_ch)./OM.^2 + x(2+jj+2*n_ch);
-        err_vec_old = @(x)err_vec_ii(x);
-        err_vec_ii = @(x) [err_vec_old(x); G_exp(mode(jj).loc(ii)-f_in_range*range : mode(jj).loc(ii)+f_in_range*range,jj) ...
-            - G_num_list{ii,jj}(x,OM_vect(mode(jj).loc(ii)-f_in_range*range : mode(jj).loc(ii) + ...
+    for jj = 1:n.ch % For each pair given (channel)
+        G_num_list{ii, jj} = @(x, OM) x(2+jj)./(-OM.^2 + 1i*2*x(2)*x(1).*OM+x(1)^2) + x(2+jj+n.ch)./OM.^2 + x(2+jj+2*n.ch);
+        err_vec_ii = @(x) [err_vec_ii(x); G_exp(mode(ii).i_nat-f_in_range*range : mode(ii).i_nat+f_in_range*range,jj) ...
+            - G_num_list{ii,jj}(x,OM_vect(mode(ii).i_nat-f_in_range*range : mode(ii).i_nat + ...
             f_in_range*range))']; % global error vector of mode ii
         x0_ii(2+jj)   = FRF_list{jj}(x0_ii(1))*2*x0_ii(2)*(x0_ii(1)).^2*1i;
-        x0_ii(2+jj+n_ch) = 0;
-        x0_ii(2+jj+2*n_ch) = 0;
+        x0_ii(2+jj+n.ch) = 0;
+        x0_ii(2+jj+2*n.ch) = 0;
     end
-
-    % err_ii = @(x) err_vec_ii(x)'*err_vec_ii(x);
     x_0s  (:,ii) = x0_ii;
     x_sols(:,ii) = lsqnonlin(err_vec_ii, x0_ii);
-    err_vec(:,ii) = err_vec_ii(x_sols(:,ii));
+    % err_vec(:,ii) = err_vec_ii(x_sols(:,ii));
 end
 
 % Now i have a solution for each mode, the global FRF is the sum of the
 % G_num of a pair, summed
-n.ch = n_ch;
-n.modes = n_modes;
 end
